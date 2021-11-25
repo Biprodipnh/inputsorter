aae74ba0c6da272a7cf282fa1a76c43b20ba6e65
Biprodip Sadhukhan <68503341+Biprodipnh@users.noreply.github.com>
2021-11-25 21:03:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ function myfunction(){
     const names = [x,y,z,p,q];
     let sorted_names = names.sort()
     document.getElementById("op").innerHTML = sorted_names;
+    //for color changing checkbox 
+    if (document.getElementById("cbox").checked) {
+        document.getElementById('op').style.backgroundColor='Green';
+    }
+    else {
+        document.getElementById('op').style.backgroundColor='Crimson';
+    }
 }
 
-function colorchange(){
-    document.getElementById('op').style.backgroundColor='Green';
-}
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
