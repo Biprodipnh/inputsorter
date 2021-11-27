19d892365e3c7d9e411f10aef5bf0e2543302c25
Biprodip Sadhukhan <68503341+Biprodipnh@users.noreply.github.com>
2021-11-27 19:15:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function myfunction(){
     var z = document.getElementById("name3").value;
     var p = document.getElementById("name4").value;
     var q = document.getElementById("name5").value;
-    const names = [x,y,z,p,q];
-    let sorted_names = names.sort()
-    document.getElementById("op").innerHTML = sorted_names;
+    //let names = [x,y,z,p,q];
+    const names = [];
+    names.push(x,y,z,p,q);
+    //for (let i = 0; i < 6; i++) {
+    //    names.push(document.getElementById("form1").value);
+    //}
+    var sorted_names = names.sort().join(" ");
+    document.getElementById("op").innerHTML = sorted_names.toUpperCase();
     //for color changing checkbox 
     if (document.getElementById("cbox").checked) {
         document.getElementById('op').style.backgroundColor='Green';
@@ -31,3 +36,13 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
+
+var count = 5;  
+function add_field() {
+    count += 1;
+    html = '<label for="name">Name:</label>\
+    <input id="name'+count+'" type="text" name="name" placeholder="Name" required>\
+    <br>'
+    var form1 = document.getElementById('form1');
+    form1.innerHTML+=html;
+}
