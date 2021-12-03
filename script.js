178c1a32ce8639957a2b9706542824f382dc8cd8
Biprodip Sadhukhan <68503341+Biprodipnh@users.noreply.github.com>
2021-12-03 17:11:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+var typed = new Typed(".auto-input", {
+    strings: ["Hello there", "Welcome to my webpage", "I'm Biprodip"],
+    typeSpeed:100,
+    backspeed:100,
+    loop:true
+})
